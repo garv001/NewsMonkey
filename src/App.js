558b11534253547b1c86801cf99859d1de53c4a4
garv001 <garvkrsingh100@gmail.com>
2024-01-11 17:05:55 +0530

    Added time, author, source

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import './App.css';
 import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 export default class App extends Component {
-  c = 'John';
+  pageSize = 5;
   render() {
     return (
       <div>
+        <Router>
         <NavBar/>
-        <News pageSize={5} country="in" category="science"/>
+        <Routes>
+          <Route path="/" element={<News key='general' pageSize={this.pageSize} country="in" category="general"/>}></Route>
+          <Route path="/business" element={<News key='business' pageSize={this.pageSize} country="in" category="business"/>}></Route>
+          <Route path="/entertainment" element={<News key='entertainment' pageSize={this.pageSize} country="in" category="entertainment"/>}></Route>
+          <Route path="/health" element={<News key='health' pageSize={this.pageSize} country="in" category="health"/>}></Route>
+          <Route path="/science" element={<News key='science' pageSize={this.pageSize} country="in" category="science"/>}></Route>
+          <Route path="/sports" element={<News key='sports' pageSize={this.pageSize} country="in" category="sports"/>}></Route>
+          <Route path="/technology" element={<News key='technology' pageSize={this.pageSize} country="in" category="technology"/>}></Route>
+        </Routes>
+        </Router>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
     return (
       <div className="my-3">
         <div className="card">
             <img src={!imageUrl?"https://static.toiimg.com/thumb/msid-47529300,width-1070,height-580,imgsize-110164,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">{title}</h5>
+                <h5 className="card-title">{title}<span className="badge rounded-pill text-bg-success">{source}</span></h5>
                 <p className="card-text">{description}</p>
+                <p className="card-text"><small className="text-body-secondary">By {author?author:"Unknown"} on {new Date(date).toGMTString()}</small></p>
                 <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read more</a>
             </div>
         </div>
