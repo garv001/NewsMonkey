c7f2fe694c8ab391f41198f2ce6050533962a17b
garv001 <garvkrsingh100@gmail.com>
2024-01-12 13:44:03 +0530

    Added top loading bar

--- src/components/News.js
+++ src/components/News.js
@@ -31,15 +31,19 @@ export class News extends Component {
     document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
   }
   async updateNews() {
+    this.props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=252bd5fc373946c1a4e91eb74ea76904&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parsedData = await data.json();
+    this.props.setProgress(70);
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading: false,
     });
+    this.props.setProgress(100);
   }
 
   async componentDidMount() {
